4ba278bf30c3b5c8d6ebeda55339c079f659049a
oka_shogo <cocorusakana@gmail.com>
2020-07-22 00:57:43 +0900

    fieldのwall作成

--- src/index.tsx
+++ src/index.tsx
@@ -13,32 +13,31 @@ const Box : React.FC<{color:string}> = (props) => {
   return <img src={props.color}/>;
 }
 
-// const Wall : React.FC = () => {
-//   return <div className="wall"></div>
-// }
+const Wall : React.FC = () => {
+  return <div className="wall"></div>
+}
 
 console.log("start");
-// const rows : Number[] = [1,2,3,4,5,6,7,8];
+
 const fieldIds : Number[] = [];
-for (let i = 1; i <= 12; i++) {
+for (let i = 1; i <= 13; i++) {
   for (let j = 1; j <= 8; j++) {
      fieldIds.push(i*10 + j);
   }
 }
 
 const Field : React.FC = () => {
-  // const list = columns.map((i) =>{
-  //   rows.map((j) => {
-  //     return(<div key = {i.toString() + j.toString()} id = {i.toString() + j.toString()}></div>);
-  //   });
-  // });
-  const fieldList = fieldIds.map((fieldId) => {
+  const fieldList = fieldIds.map((fieldId: Number) => {
+    if(parseInt(fieldId.toString()) % 10 == 1 || parseInt(fieldId.toString()) % 10 == 8){
+      return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
+    }
+    else if(Math.floor(parseInt(fieldId.toString()) / 10) == 13){
+      return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
+    }
+    else{
       return(<div key = {fieldId.toString()} id = {fieldId.toString()}></div>);
+    }
   });
-  // const list : Element[] = [];
-  // for (let i = 0; i < 8; i++) {
-  //     list.push(<div key = {i.toString()} id = {i.toString()}></div>);
-  // };
   return <div className="field">{fieldList}</div>;
 }
 
@@ -71,70 +70,13 @@ class App extends React.Component {
     );
   }
 }
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <div className="nextField">
-//           <div className="nextField1">
-//             <h3>next1</h3>
-//             <Box color={B} />
-//             <Box color={R} />
-//           </div>
-//           <div className="nextField2">
-//             <h3>next2</h3>
-//             <Box color={B} />
-//             <Box color={R} />
-//           </div>
-//         </div>
-//         <div className="field">
-//           <div id = "11">
-//             <Wall />
-//           </div>
-//           <div id = "12">
-//             <Box color={R} />
-//           </div>
-//           <div id = "13">
-//             <Box color={B} />
-//           </div>
-//           <div id = "14">
-//             <Box color={R} />
-//           </div>
-//           <div id = "15">
-//             <Box color={B} />
-//           </div>
-//           <div id = "16">
-//             <Box color={R} />
-//           </div>
-//           <div id = "17">
-//             <Box color={B} />
-//           </div>
-//           <div id = "18">
-//             <Wall />
-//           </div>
-//           <div id = "21">
-//           </div>
-//           <div id = "22">
-//           </div>
-//           <div id = "23">
-//           </div>
-//           <div id = "24">
-//           </div>
-//           <div id = "25">
-//           </div>
-//           <div id = "26">
-//           </div>
-//           <div id = "27">
-//           </div>
-//           <div id = "28">
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 ReactDOM.render(
   <App />,
   document.getElementById("app")
+);
+
+ReactDOM.render(
+  <Wall />,
+  document.getElementById("11")
 );
