6117093e3abe98d6d48bb8e30746a8a84d3975f3
oka_shogo <cocorusakana@gmail.com>
2020-07-22 22:50:16 +0900

    Wallの作成

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import './index.css';
 // import G from './img/G.jpg'
 const B = require('./img/B.jpg').default;
 const R = require('./img/R.jpg').default;
+// const G = require('./img/G.jpg').default;
+const W = require('./img/W.jpg').default;
 
 // console.log(B);
 // import Y from './img/Y.jpg'
@@ -22,13 +24,13 @@ class Box extends React.Component<{color:string}> {
   }
 }
 
-class Wall extends React.Component {
-  render() {
-    return (
-      <div className="wall"></div>
-    );
-  }
-}
+// class Wall extends React.Component {
+//   render() {
+//     return (
+//       <div className="wall"></div>
+//     );
+//   }
+// }
 
 console.log("start");
 
@@ -40,31 +42,80 @@ for (let i = 1; i <= 13; i++) {
 }
 
 // Fieldを描画
-const Field : React.FC = () => {
-  const fieldList = fieldIds.map((fieldId: Number) => {
-    // 左右に壁
-    if(parseInt(fieldId.toString()) % 10 == 1 || parseInt(fieldId.toString()) % 10 == 8){
-      return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
-    }
-    // 下に壁
-    else if(Math.floor(parseInt(fieldId.toString()) / 10) == 13){
-      return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
-    }
-    // それ以外は空
-    else{
-      return(<div key = {fieldId.toString()} id = {fieldId.toString()}></div>);
-    }
-  });
-  return <div className="field">{fieldList}</div>;
+interface IState {
+  squares : String[];
+  nowpuyo : Number;
 }
 
-// const Walls : React.FC = () => {
-//   const rows = [1,2,3,4,5,6].map((key) =>
-//     <div id={key.toString()} className="wall"></div>
-//   );
-//   return <div>{rows}</div>
-// }
+class Field extends React.Component<{}, IState> {
+  constructor(props : string[]) {
+    super(props);
+    this.state = {
+      squares: Array(8*13).fill(B),
+      nowpuyo: 10,
+    };
+    for (let i = 0; i < 12; i++) {
+      this.state.squares[0 + i*8] = W
+      this.state.squares[7 + i*8] = W
+    }
+    for (let i = 0; i < 8; i++) {
+      this.state.squares[8*12 + i] = W
+    }
 
+  }
+
+  // renderSquare(i: Number) {
+  //   return (
+  //     <Box
+  //       color={this.state.squares[i]}
+  //     />
+  //   );
+  // }
+
+  render(){
+    const fieldList = this.state.squares.map((output: string, key) => {
+      return(
+        <div key = {key.toString()} id = {key.toString()} onClick={() => alert("aaa")}><Box color={output}/></div>
+      )
+    });
+    // const fieldList = fieldIds.map((fieldId: Number) => {
+    //   // 左右に壁
+    //   if(parseInt(fieldId.toString()) % 10 == 1 || parseInt(fieldId.toString()) % 10 == 8){
+    //     return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
+    //   }
+    //   // 下に壁
+    //   else if(Math.floor(parseInt(fieldId.toString()) / 10) == 13){
+    //     return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
+    //   }
+    //   // それ以外は空
+    //   else{
+    //     return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Box color={B}/></div>);
+    //   }
+    // });
+    return (
+      <div className="field">
+        {fieldList}
+      </div>
+    );
+  }
+}
+// const Field : React.FC = () => {
+//   const fieldList = fieldIds.map((fieldId: Number) => {
+//     // 左右に壁
+//     if(parseInt(fieldId.toString()) % 10 == 1 || parseInt(fieldId.toString()) % 10 == 8){
+//       return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
+//     }
+//     // 下に壁
+//     else if(Math.floor(parseInt(fieldId.toString()) / 10) == 13){
+//       return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
+//     }
+//     // それ以外は空
+//     else{
+//       return(<div key = {fieldId.toString()} id = {fieldId.toString()}></div>);
+//     }
+//   });
+//   return <div className="field">{fieldList}</div>;
+// }
 
 class App extends React.Component {
   render() {
@@ -92,3 +143,11 @@ ReactDOM.render(
   <App />,
   document.getElementById("app")
 );
+
+window.addEventListener("keydown",keydown);
+
+function keydown(event:any){
+  if(event.keyCode==40){alert("下")}
+  if(event.keyCode==39){alert("右")}
+  if(event.keyCode==37){alert("左")}
+}
