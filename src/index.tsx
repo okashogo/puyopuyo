ff6b00278c7d99ef5d8e3bc5f75befbe53fee89d
oka_shogo <cocorusakana@gmail.com>
2020-07-23 11:21:38 +0900

    subpuyoの作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
-// import R from './img/R.jpg'
-// import G from './img/G.jpg'
+
 const B = require('./img/B.jpg').default;
 const R = require('./img/R.jpg').default;
 const G = require('./img/G.jpg').default;
+const P = require('./img/P.jpg').default;
+const Y = require('./img/Y.jpg').default;
 const W = require('./img/W.jpg').default;
-
-// console.log(B);
-// import Y from './img/Y.jpg'
-// import P from './img/P.jpg'
-
-// const Box : React.FC<{color:string}> = (props) => {
-//     return <img src={props.color}/>;
-// }
 
 class Box extends React.Component<{color:string}> {
   render() {
@@ -32,20 +25,13 @@ class Box extends React.Component<{color:string}> {
   }
 }
 
-// class Wall extends React.Component {
-//   render() {
-//     return (
-//       <div className="wall"></div>
-//     );
-//   }
-// }
-
 console.log("start");
 
 // Fieldを描画
 interface IState {
   squares : string[];
   nowpuyo : number;
+  subpuyo : number;
 }
 
 class Field extends React.Component<{}, IState> {
@@ -53,7 +39,8 @@ class Field extends React.Component<{}, IState> {
     super(props);
     this.state = {
       squares: Array(8*13).fill(null),
-      nowpuyo: 3,
+      nowpuyo: 3+8,
+      subpuyo: 3,
     };
     for (let i = 0; i < 12; i++) {
       this.state.squares[0 + i*8] = W
@@ -64,6 +51,7 @@ class Field extends React.Component<{}, IState> {
     }
 
     this.state.squares[this.state.nowpuyo] = G
+    this.state.squares[this.state.subpuyo] = R
 
   }
 
@@ -82,12 +70,23 @@ class Field extends React.Component<{}, IState> {
     if(e.keyCode==37){
       shiftNum = -1;
     }
-    const tmpSquares = this.state.squares;
-    const tmpNowPuyo = this.state.nowpuyo;
-    tmpSquares[tmpNowPuyo] =  "";
-    this.setState({nowpuyo: this.state.nowpuyo + shiftNum});
-    tmpSquares[this.state.nowpuyo] =  G;
-    this.setState({squares: tmpSquares});
+    if(shiftNum != 0){
+      if(this.state.squares[this.state.nowpuyo + shiftNum] == W || this.state.squares[this.state.nowpuyo + shiftNum] == W){
+          return;
+      }
+      const tmpSquares = this.state.squares;
+      const tmpNowPuyo = this.state.nowpuyo;
+      const tmpSubPuyo = this.state.subpuyo;
+      const tmpNowPuyoColor = this.state.squares[tmpNowPuyo];
+      const tmpSubPuyoColor = this.state.squares[tmpSubPuyo];
+      this.setState({nowpuyo: this.state.nowpuyo + shiftNum});
+      this.setState({subpuyo: this.state.subpuyo + shiftNum});
+      tmpSquares[tmpNowPuyo] =  "";
+      tmpSquares[tmpSubPuyo] =  "";
+      tmpSquares[this.state.nowpuyo] =  tmpNowPuyoColor;
+      tmpSquares[this.state.subpuyo] =  tmpSubPuyoColor;
+      this.setState({squares: tmpSquares});
+    }
   }
 
   render(){
@@ -96,20 +95,6 @@ class Field extends React.Component<{}, IState> {
         <div key = {key.toString()} id = {key.toString()}><Box color={output}/></div>
       )
     });
-    // const fieldList = fieldIds.map((fieldId: Number) => {
-    //   // 左右に壁
-    //   if(parseInt(fieldId.toString()) % 10 == 1 || parseInt(fieldId.toString()) % 10 == 8){
-    //     return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
-    //   }
-    //   // 下に壁
-    //   else if(Math.floor(parseInt(fieldId.toString()) / 10) == 13){
-    //     return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
-    //   }
-    //   // それ以外は空
-    //   else{
-    //     return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Box color={B}/></div>);
-    //   }
-    // });
     return (
       <div className="field">
         {fieldList}
@@ -117,23 +102,6 @@ class Field extends React.Component<{}, IState> {
     );
   }
 }
-// const Field : React.FC = () => {
-//   const fieldList = fieldIds.map((fieldId: Number) => {
-//     // 左右に壁
-//     if(parseInt(fieldId.toString()) % 10 == 1 || parseInt(fieldId.toString()) % 10 == 8){
-//       return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
-//     }
-//     // 下に壁
-//     else if(Math.floor(parseInt(fieldId.toString()) / 10) == 13){
-//       return(<div key = {fieldId.toString()} id = {fieldId.toString()}><Wall /></div>);
-//     }
-//     // それ以外は空
-//     else{
-//       return(<div key = {fieldId.toString()} id = {fieldId.toString()}></div>);
-//     }
-//   });
-//   return <div className="field">{fieldList}</div>;
-// }
 
 class App extends React.Component {
   render() {
@@ -147,8 +115,8 @@ class App extends React.Component {
           </div>
           <div className="nextField2">
             <h3>next2</h3>
-            <Box color={B} />
-            <Box color={R} />
+            <Box color={Y} />
+            <Box color={P} />
           </div>
         </div>
         <Field />
@@ -161,17 +129,3 @@ ReactDOM.render(
   <App />,
   document.getElementById("app")
 );
-
-// window.addEventListener("keydown",keydown);
-//
-// function keydown(event:any){
-//   if(event.keyCode==40){
-//     alert("下")
-//   }
-//   if(event.keyCode==39){
-//     alert("右")
-//   }
-//   if(event.keyCode==37){
-//     alert("左")
-//   }
-// }
